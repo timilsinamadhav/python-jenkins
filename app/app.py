990b73345947f18f13afc6a1b0dcf17ff3f4e976
Madhav Timilsina <mti@logpoint.com>
2024-04-12 08:31:32 +0545

    another test

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Devops Class, Welcome to Broadway! This is basic flask app!'
+    return 'Hello Devops Class, Welcome to Broadway! This is basic flask app! this is next build'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
